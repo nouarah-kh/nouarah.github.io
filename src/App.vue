4f46307cf48480b104ed11bac14047399836b063
Nouarah Khalfallah <n.khalfallah@email.ly>
2023-02-21 15:59:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <QrCard />
+    <FrontMentorAttribution />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import QrCard from '@/components/QrCard.vue';
+import FrontMentorAttribution from '@/components/FrontMentorAttribution.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    QrCard,
+    FrontMentorAttribution,
+  },
+});
+</script>
+
---
+++ src/components/FrontMentorAttribution.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="attribution">
+    <span>Challenge by &nbsp;</span>
+    <a :href="frontMentorUrl" target="_blank"> Frontend Mentor</a>. Coded by
+    <a href="#">{{ codedBy }}</a
+    >.
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      frontMentorUrl: 'https://www.frontendmentor.io?ref=challenge',
+      codedBy: 'Nouarah Khalfallah',
+    };
+  },
+});
+</script>
