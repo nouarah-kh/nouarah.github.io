8bb5d4b7ef238f1122b3cfcbde1650e1896a8bc6
Nouarah Khalfallah <nr.khalfallah@gmail.com>
2023-02-22 12:16:11 +0200

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "nouarah.github.io" : "/",
   };
   
