6ef1fecd60ee6ccc1cee951dd8880d1a7bbfb2be
Nouarah Khalfallah <nr.khalfallah@gmail.com>
2023-02-23 09:16:32 +0200

    improve solution

--- src/components/card.vue
+++ src/components/card.vue
@@ -4,7 +4,7 @@
       <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin-remove-first-child uk-width-large uk-margin-auto uk-text-center">
           <img class="uk-border-rounded" src="/images/image-qr-code.png" alt="Code QR" />
         <div class="uk-card-body uk-padding-small uk-margin-remove-first-child">
-          <h3 class="uk-card-title uk-text-bold">Improve your front-end skills by building projects</h3>
+          <h1 class="uk-card-title uk-text-bold">Improve your front-end skills by building projects</h1>
           <div class="uk-panel uk-margin-top">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level.
